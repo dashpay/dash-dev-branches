d688f51aab737f3af7626b6da91b8b250791fa2f
Alexander Block <ablock84@gmail.com>
2018-02-12 14:44:32 +0100

    DIP2 changes to CTransaction and CMutableTransaction

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -70,6 +70,7 @@ void TxToJSON(const CTransaction& tx, const uint256 hashBlock, UniValue& entry)
     entry.push_back(Pair("txid", txid.GetHex()));
     entry.push_back(Pair("size", (int)::GetSerializeSize(tx, SER_NETWORK, PROTOCOL_VERSION)));
     entry.push_back(Pair("version", tx.nVersion));
+    entry.push_back(Pair("type", tx.nType));
     entry.push_back(Pair("locktime", (int64_t)tx.nLockTime));
     UniValue vin(UniValue::VARR);
     BOOST_FOREACH(const CTxIn& txin, tx.vin) {
@@ -125,6 +126,11 @@ void TxToJSON(const CTransaction& tx, const uint256 hashBlock, UniValue& entry)
         vout.push_back(out);
     }
     entry.push_back(Pair("vout", vout));
+
+    if (!tx.extraPayload.empty()) {
+        entry.push_back(Pair("extraPayloadSize", tx.extraPayload.size()));
+        entry.push_back(Pair("extraPayload", HexStr(tx.extraPayload)));
+    }
 
     if (IsSubTx(tx)) {
         UniValue subTxValue(UniValue::VOBJ);
@@ -511,6 +517,7 @@ UniValue decoderawtransaction(const JSONRPCRequest& request)
             "  \"txid\" : \"id\",        (string) The transaction id\n"
             "  \"size\" : n,             (numeric) The transaction size\n"
             "  \"version\" : n,          (numeric) The version\n"
+            "  \"type\" : n,             (numeric) The type\n"
             "  \"locktime\" : ttt,       (numeric) The lock time\n"
             "  \"vin\" : [               (array of json objects)\n"
             "     {\n"
