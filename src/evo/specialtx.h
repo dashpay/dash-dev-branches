6a75f41df26099723d46c8c327ae5065fe5378ce
Alexander Block <ablock84@gmail.com>
2018-02-14 12:05:25 +0100

    Add GetTxPayload helpers

--- src/evo/specialtx.h
+++ src/evo/specialtx.h
@@ -5,6 +5,9 @@
 #ifndef DASH_SPECIALTX_H
 #define DASH_SPECIALTX_H
 
+#include "streams.h"
+#include "clientversion.h"
+
 class CTransaction;
 class CBlock;
 class CBlockIndex;
@@ -14,6 +17,26 @@ bool CheckSpecialTx(const CTransaction& tx, const CBlockIndex *pindex, CValidati
 bool ProcessSpecialTxsInBlock(const CBlock &block, const CBlockIndex *pindex, CValidationState &state);
 bool UndoSpecialTxsInBlock(const CBlock &block, const CBlockIndex *pindex);
 
+template <typename T>
+inline bool GetTxPayload(const std::vector<unsigned char> &payload, T &obj) {
+    CDataStream ds(payload, CLIENT_VERSION, SER_NETWORK);
+    try {
+        ds >> obj;
+    } catch (std::exception &e) {
+        return false;
+    }
+    if (!ds.empty())
+        return false;
+    return true;
+}
+template <typename T>
+inline bool GetTxPayload(const CMutableTransaction &tx, T &obj) {
+    return GetTxPayload(tx.extraPayload, obj);
+}
+template <typename T>
+inline bool GetTxPayload(const CTransaction &tx, T &obj) {
+    return GetTxPayload(tx.extraPayload, obj);
+}
 
 uint256 CalcTxInputsHash(const CTransaction &tx);
 
