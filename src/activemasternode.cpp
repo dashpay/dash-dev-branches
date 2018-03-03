3bf35b7dbcc56fc54a41e5d12bd403d53017d3c4
Alexander Block <ablock84@gmail.com>
2018-03-03 15:33:26 +0100

    Update masternode outpoint in case a MNB arrives with a changed outpoint
    Allows to broadcast a MNB while the MN is already running and update the
    collateral in case it has changed.

--- src/activemasternode.cpp
+++ src/activemasternode.cpp
@@ -386,6 +386,7 @@ void CActiveLegacyMasternodeManager::ManageStateRemote()
                 LogPrintf("CActiveLegacyMasternodeManager::ManageStateRemote -- %s: %s\n", GetStateString(), strNotCapableReason);
                 return;
             }
+            LogPrintf("CActiveLegacyMasternodeManager::ManageStateRemote -- Collateral is a ProTx\n");
         }
         if(nState != ACTIVE_MASTERNODE_STARTED) {
             LogPrintf("CActiveLegacyMasternodeManager::ManageStateRemote -- STARTED!\n");
@@ -393,6 +394,9 @@ void CActiveLegacyMasternodeManager::ManageStateRemote()
             activeMasternode.service = infoMn.addr;
             fPingerEnabled = true;
             nState = ACTIVE_MASTERNODE_STARTED;
+        } else if (activeMasternode.outpoint != infoMn.outpoint) {
+            LogPrintf("CActiveLegacyMasternodeManager::ManageStateRemote -- STARTED with new collateral %s!\n", infoMn.outpoint.ToStringShort());
+            activeMasternode.outpoint = infoMn.outpoint;
         }
     }
     else {
