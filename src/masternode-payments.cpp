61a5f8b7550489317c696f3e3a6caac5730e427f
Alexander Block <ablock84@gmail.com>
2018-03-09 12:46:27 +0100

    Implement deterministic version of CMasternodePayments::IsScheduled
    Needed for privatesend when choosing masternodes

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -591,6 +591,16 @@ bool CMasternodePayments::IsScheduled(const masternode_info_t& mnInfo, int nNotB
 {
     LOCK(cs_mapMasternodeBlocks);
 
+    if (deterministicMNManager->IsDeterministicMNsSporkActive()) {
+        auto projectedPayees = deterministicMNManager->GetListAtChainTip().GetProjectedMNPayees(8);
+        for (const auto &dmn : projectedPayees) {
+            if (dmn->proTxHash == mnInfo.outpoint.hash) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     if(!masternodeSync.IsMasternodeListSynced()) return false;
 
     CScript mnpayee;
