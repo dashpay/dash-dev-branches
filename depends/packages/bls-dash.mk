c8ac2f329ab5af50677835becda3cfb6805d4df4
Kittywhiskers Van Gogh <63189531+kittywhiskers@users.noreply.github.com>
2022-10-12 13:37:15 +0530

    build(bls): bump bls-dash depends to latest develop (4da22c2557e39d85c54a1d6de1ec31c41493b8e8) (#4)

--- depends/packages/bls-dash.mk
+++ depends/packages/bls-dash.mk
@@ -1,34 +1,22 @@
 package=bls-dash
-$(package)_version=1.2.0
+$(package)_version=4da22c2557e39d85c54a1d6de1ec31c41493b8e8
 $(package)_download_path=https://github.com/dashpay/bls-signatures/archive
 $(package)_download_file=$($(package)_version).tar.gz
 $(package)_file_name=$(package)-$($(package)_download_file)
 $(package)_build_subdir=build
-$(package)_sha256_hash=94e49f3eaa29bc1f354cd569c00f4f4314d1c8ab4758527c248b67da9686135a
+$(package)_sha256_hash=989c9670855cdcbab4b425eb794f65d4bc9fa59131cb65bde00f42943a620d51
 $(package)_dependencies=gmp cmake
 $(package)_darwin_triplet=x86_64-apple-darwin19
 
-$(package)_relic_version=aecdcae7956f542fbee2392c1f0feb0a8ac41dc5
-$(package)_relic_download_path=https://github.com/relic-toolkit/relic/archive
-$(package)_relic_download_file=$($(package)_relic_version).tar.gz
-$(package)_relic_file_name=relic-$($(package)_relic_download_file)
-$(package)_relic_build_subdir=relic
-$(package)_relic_sha256_hash=f2de6ebdc9def7077f56c83c8b06f4da5bacc36b709514bd550a92a149e9fa1d
-
-$(package)_extra_sources=$($(package)_relic_file_name)
-
 define $(package)_fetch_cmds
-$(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash)) && \
-$(call fetch_file,$(package),$($(package)_relic_download_path),$($(package)_relic_download_file),$($(package)_relic_file_name),$($(package)_relic_sha256_hash))
+$(call fetch_file,$(package),$($(package)_download_path),$($(package)_download_file),$($(package)_file_name),$($(package)_sha256_hash))
 endef
 
 define $(package)_extract_cmds
   mkdir -p $($(package)_extract_dir) && \
   echo "$($(package)_sha256_hash)  $($(package)_source)" > $($(package)_extract_dir)/.$($(package)_file_name).hash && \
-  echo "$($(package)_relic_sha256_hash)  $($(package)_source_dir)/$($(package)_relic_file_name)" >> $($(package)_extract_dir)/.$($(package)_file_name).hash && \
   $(build_SHA256SUM) -c $($(package)_extract_dir)/.$($(package)_file_name).hash && \
-  tar --strip-components=1 -xf $($(package)_source) -C . && \
-  cp $($(package)_source_dir)/$($(package)_relic_file_name) .
+  tar --strip-components=1 -xf $($(package)_source) -C .
 endef
 
 define $(package)_set_vars
@@ -52,11 +40,6 @@ define $(package)_set_vars
   endif
 
   $(package)_cppflags+=-UBLSALLOC_SODIUM
-endef
-
-define $(package)_preprocess_cmds
-  sed -i.old "s|GIT_REPOSITORY https://github.com/Chia-Network/relic.git|URL \"../../relic-$($(package)_relic_version).tar.gz\"|" CMakeLists.txt && \
-  sed -i.old "s|RELIC_GIT_TAG \".*\"|RELIC_GIT_TAG \"\"|" CMakeLists.txt
 endef
 
 define $(package)_config_cmds
