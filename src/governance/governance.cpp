9cbb1b48b40d56496a90a29450ec0c55fc1e5fc0
Odysseas Gabrielides <odysseas.gabrielides@gmail.com>
2023-09-04 19:39:13 +0300

    fix

--- src/governance/governance.cpp
+++ src/governance/governance.cpp
@@ -310,7 +310,7 @@ void CGovernanceManager::AddGovernanceObject(CGovernanceObject& govobj, CConnman
     // SEND NOTIFICATION TO SCRIPT/ZMQ
     GetMainSignals().NotifyGovernanceObject(std::make_shared<const CGovernanceObject>(govobj));
 
-    if (govobj.GetObjectType() == GovernanceObject::TRIGGER && HasAlreadyVotedFundingTrigger()) {
+    if (govobj.GetObjectType() == GovernanceObject::TRIGGER) {
         vote_outcome_enum_t funding_vote = HasAlreadyVotedFundingTrigger() ? VOTE_OUTCOME_NO : VOTE_OUTCOME_YES;
         VoteFundingTrigger(govobj.GetHash(), funding_vote, connman);
     }
