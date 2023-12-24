96c55ea87b26e6b6329b0336dfa5bf6dfc079c7e
UdjinM6 <UdjinM6@users.noreply.github.com>
2023-12-24 15:43:33 +0300

    Revert "fix: partially revert changes in `git_head_version`s"
    This reverts commit 9c4778403bc72cf1578bc757a19a1812bf90c5b9.

--- contrib/shell/git-utils.bash
+++ contrib/shell/git-utils.bash
@@ -5,10 +5,6 @@ git_root() {
 }
 
 git_head_version() {
-    local recent_tag
-    if recent_tag="$(git describe --abbrev=12 --dirty 2> /dev/null)"; then
-        echo "${recent_tag#v}"
-    else
-        git -C "$1" rev-parse --short=12 HEAD
-    fi
+    recent_tag="$(git describe --abbrev=12 --dirty 2> /dev/null)"
+    echo "${recent_tag#v}"
 }
