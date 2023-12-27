133a14ea115a35e186095aa946b05837cdde7483
Konstantin Akimov <knstqq@gmail.com>
2023-12-27 19:24:23 +0700

    refactor: new module llmq/options for reduce using llmq/utils externally

---
+++ src/llmq/options.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2018-2023 The Dash Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef BITCOIN_LLMQ_OPTIONS_H
+#define BITCOIN_LLMQ_OPTIONS_H
+
+
+
+
+#endif // BITCOIN_LLMQ_OPTIONS_H
