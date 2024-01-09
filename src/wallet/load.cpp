knst/dash

refactor: remove dependency wallet/load on CoinJoin by creating new interface method for flush

src/wallet/load.cpp
@@ -141,10 +141,7 @@ void FlushWallets()
     for (const std::shared_ptr<CWallet>& pwallet : GetWallets()) {
         if (CCoinJoinClientOptions::IsEnabled()) {
             // Stop CoinJoin, release keys
-            auto cj_clientman = ::coinJoinClientManagers->Get(*pwallet);
-            assert(cj_clientman != nullptr);
-            cj_clientman->ResetPool();
-            cj_clientman->StopMixing();
+            pwallet->chain().cjFlushWallet(*pwallet);
         }
         pwallet->Flush();
     }

dashpay/dash-dev-branches

src/wallet/load.cpp
@@ -141,10 +141,7 @@ void FlushWallets()
     for (const std::shared_ptr<CWallet>& pwallet : GetWallets()) {
         if (CCoinJoinClientOptions::IsEnabled()) {
             // Stop CoinJoin, release keys
-            auto cj_clientman = ::coinJoinWalletManager->Get(*pwallet);
-            assert(cj_clientman != nullptr);
-            cj_clientman->ResetPool();
-            cj_clientman->StopMixing();
+            pwallet->coinjoin_loader().FlushWallet(*pwallet);
         }
         pwallet->Flush();
     }