606ad66af889ee50a3d33d852fc0aa0b6e2a3332
Konstantin Akimov <knstqq@gmail.com>
2024-09-17 11:49:36 +0700

    feat: new single node quorum and functional test feature_llmq_singlenode.py

--- src/llmq/dkgsessionhandler.cpp
+++ src/llmq/dkgsessionhandler.cpp
@@ -549,6 +549,18 @@ void CDKGSessionHandler::HandleDKGRound(CConnman& connman, PeerManager& peerman)
         return changed;
     });
 
+    if (params.size == 1) {
+        LogPrint(BCLog::LLMQ_DKG, "CDKGSessionManager::%s -- %s qi[%d] h=%s - finalize-single-commitment \n", __func__, params.name, quorumIndex, quorumHash.ToString());
+        auto finalCommitment = curSession->FinalizeSingleCommitment();
+        if (!finalCommitment.IsNull()) { // it can be null only if we are not member
+            if (auto inv_opt = quorumBlockProcessor.AddMineableCommitment(finalCommitment); inv_opt.has_value()) {
+                peerman.RelayInv(inv_opt.value());
+            }
+        }
+        WaitForNextPhase(QuorumPhase::Initialized, QuorumPhase::Contribute, curQuorumHash);
+        return;
+    }
+
     const auto tip_mn_list = m_dmnman.GetListAtChainTip();
     utils::EnsureQuorumConnections(params, connman, m_dmnman, m_sporkman, m_qsnapman, tip_mn_list, pQuorumBaseBlockIndex,
                                    curSession->myProTxHash, /* is_masternode = */ m_mn_activeman != nullptr);
