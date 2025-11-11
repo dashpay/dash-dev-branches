9e3182b53c7d9d86d1f3998798f6eb09b81ec854
pasta <pasta@dashboost.org>
2025-11-10 21:35:53 -0600

    refactor: improve logging in CInstantSendManager
    - Updated log messages in CInstantSendManager to enhance clarity and maintain consistency.
    - Continued log message formatting for better readability.
    - No functional changes were made to the logic.

--- src/instantsend/instantsend.cpp
+++ src/instantsend/instantsend.cpp
@@ -155,8 +155,8 @@ std::variant<uint256, CTransactionRef, std::monostate> CInstantSendManager::Proc
             // Let's see if the TX that was locked by this islock is already mined in a ChainLocked block. If yes,
             // we can simply ignore the islock, as the ChainLock implies locking of all TXs in that chain
             if (clhandler.HasChainLock(minedHeight, hashBlock)) {
-                LogPrint(BCLog::INSTANTSEND,
-                         "CInstantSendManager::%s -- txlock=%s, islock=%s: dropping islock as it already got a "
+                LogPrint(BCLog::INSTANTSEND, /* Continued */
+                         "CInstantSendManager::%s -- txlock=%s, islock=%s: dropping islock as it already got a " /* Continued */
                          "ChainLock in block %s, peer=%d\n",
                          __func__, islock->txid.ToString(), hash.ToString(), hashBlock.ToString(), from);
                 return std::monostate{};
@@ -167,8 +167,8 @@ std::variant<uint256, CTransactionRef, std::monostate> CInstantSendManager::Proc
                 CacheBlockHeight(pindexMined->GetBlockHash(), pindexMined->nHeight);
                 minedHeight = pindexMined->nHeight;
                 if (clhandler.HasChainLock(minedHeight, pindexMined->GetBlockHash())) {
-                    LogPrint(BCLog::INSTANTSEND,
-                             "CInstantSendManager::%s -- txlock=%s, islock=%s: dropping islock as it already got a "
+                    LogPrint(BCLog::INSTANTSEND, /* Continued */
+                             "CInstantSendManager::%s -- txlock=%s, islock=%s: dropping islock as it already got a " /* Continued */
                              "ChainLock in block %s, peer=%d\n",
                              __func__, islock->txid.ToString(), hash.ToString(), hashBlock.ToString(), from);
                     return std::monostate{};
