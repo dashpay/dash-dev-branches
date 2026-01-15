edfa02395911b0528433fd27049ef301a22b8ef2
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-01-16 02:53:18 +0530

    debug: Add log that validates successful FFI invocation at init
    This can be removed alongside the whole minimal crate when we have
    actual crates to slot in.

--- src/init.cpp
+++ src/init.cpp
@@ -112,6 +112,8 @@
 #include <coinjoin/options.h>
 #endif // ENABLE_WALLET
 
+#include <rust/chirp/lib.h>
+
 #include <algorithm>
 #include <condition_variable>
 #include <cstdint>
@@ -1505,6 +1507,8 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
         // Detailed error printed inside StartLogging().
         return false;
     }
+
+    LogPrintf("%s\n", std::string(chirp::chirp()));
 
     LogPrintf("Using at most %i automatic connections (%i file descriptors available)\n", nMaxConnections, nFD);
 
