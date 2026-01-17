7892dc0b7cc079a49e60ff1db2a7947c2599aa13
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-01-17 17:16:54 +0530

    depends: integrate Rust and `cxxbridge` into depends

---
+++ depends/packages/rustcxx.mk
@@ -0,0 +1,17 @@
+# Copyright (c) 2022-2023 The Zcash developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+package:=rustcxx
+$(package)_version:=$(native_cxxbridge_version)
+$(package)_file_name:=$(native_cxxbridge_file_name)
+$(package)_sha256_hash:=$(native_cxxbridge_sha256_hash)
+
+define $(package)_fetch_cmds
+  $(call native_cxxbridge_fetch_cmds,native_cxxbridge)
+endef
+
+define $(package)_stage_cmds
+  mkdir -p $($(package)_staging_prefix_dir)/include/rust && \
+  cp include/cxx.h $($(package)_staging_prefix_dir)/include/rust
+endef
