2f9246d920212804f2f13f02696ec75e392250db
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-01-18 11:59:24 +0530

    build: switch to Rust 1.85.1, use musl stdlib to avoid Guix glibc issues
    The conflict occurs due a difference in glibc version between the Guix
    environment and the targets compiled, this is only a problem for Linux
    targets so the issue doesn't extend to Windows

--- depends/packages/native_rust.mk
+++ depends/packages/native_rust.mk
@@ -7,28 +7,28 @@
 # ./contrib/devtools/update-rust-hashes.py
 
 package:=native_rust
-$(package)_version:=1.82.0
+$(package)_version:=1.85.1
 $(package)_download_path:=https://static.rust-lang.org/dist
 
 # FreeBSD (x86_64)
 $(package)_file_name_x86_64_freebsd:=rust-$($(package)_version)-x86_64-unknown-freebsd.tar.gz
-$(package)_sha256_hash_x86_64_freebsd:=f7b51943dbed0af3387e3269c1767fee916fb22b8e7897b3594bf5e422403137
+$(package)_sha256_hash_x86_64_freebsd:=f905730e22a9a8a2dfce1ab0c50d427b7978c5b235c33018b09552041b6f6329
 
 # Linux (ARMv8)
 $(package)_file_name_aarch64_linux:=rust-$($(package)_version)-aarch64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash_aarch64_linux:=d7db04fce65b5f73282941f3f1df5893be9810af17eb7c65b2e614461fe31a48
+$(package)_sha256_hash_aarch64_linux:=d2609d8cd965060f0b4a8c509131066369e8d3d31a92fedce177b42b32af6b4d
 
 # Linux (x86_64)
 $(package)_file_name_x86_64_linux:=rust-$($(package)_version)-x86_64-unknown-linux-gnu.tar.gz
-$(package)_sha256_hash_x86_64_linux:=0265c08ae997c4de965048a244605fb1f24a600bbe35047b811c638b8fcf676b
+$(package)_sha256_hash_x86_64_linux:=b7202563a52b47f575b284a5a4794fafd688e39bfe8fd855b5e80129e671cb7f
 
 # macOS (ARMv8)
 $(package)_file_name_aarch64_darwin:=rust-$($(package)_version)-aarch64-apple-darwin.tar.gz
-$(package)_sha256_hash_aarch64_darwin:=49b6d36b308addcfd21ae56c94957688338ba7b8985bff57fc626c8e1b32f62c
+$(package)_sha256_hash_aarch64_darwin:=64b0341a47e684d648c9b7defd0b7ff9d5397a64718cf803c1e114544f94bbe9
 
 # macOS (x86_64)
 $(package)_file_name_x86_64_darwin:=rust-$($(package)_version)-x86_64-apple-darwin.tar.gz
-$(package)_sha256_hash_x86_64_darwin:=b1a289cabc523f259f65116a41374ac159d72fbbf6c373bd5e545c8e835ceb6a
+$(package)_sha256_hash_x86_64_darwin:=6e321957b7301d48e5ecf61bdeea6560400a5948b3e72830348367a8a9696ad7
 
 $(package)_file_name=$($(package)_file_name_$(build_arch)_$(build_os))
 $(package)_sha256_hash=$($(package)_sha256_hash_$(build_arch)_$(build_os))
