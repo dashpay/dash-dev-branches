4c28b281628cbde4a9daedb185d1a9be73d80524
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-01-18 11:59:24 +0530

    build: switch to Rust 1.85.1, use musl stdlib to avoid Guix glibc issues
    The conflict occurs due a difference in glibc version between the Guix
    environment and the targets compiled, this is only a problem for Linux
    targets so the issue doesn't extend to Windows

--- depends/packages/rust_stdlib.mk
+++ depends/packages/rust_stdlib.mk
@@ -7,7 +7,7 @@
 # ./contrib/devtools/update-rust-hashes.py
 
 package:=rust_stdlib
-$(package)_version:=1.82.0
+$(package)_version:=1.85.1
 $(package)_download_path:=https://static.rust-lang.org/dist
 $(package)_dependencies:=native_rust
 $(package)_target=$(or \
@@ -19,50 +19,50 @@ $(package)_target=$(or \
 # FreeBSD (x86_64)
 $(package)_targets += x86_64-unknown-freebsd
 $(package)_target_x86_64-unknown-freebsd:=x86_64-unknown-freebsd
-$(package)_sha256_hash_x86_64-unknown-freebsd:=be1acaf3c2f15d42b05b1f032db5ac3b11a0ac5a91c0efef26f2d8135d68a829
+$(package)_sha256_hash_x86_64-unknown-freebsd:=08a691bcdb5bde37178368e9e49dbd822d9e39c68b9371191bd16ab7f8b321c4
 
 # Linux (ARMv7)
-$(package)_targets += armv7-unknown-linux-gnueabihf
-$(package)_target_arm-unknown-linux-gnueabihf:=armv7-unknown-linux-gnueabihf
-$(package)_target_armv7-unknown-linux-gnueabihf:=armv7-unknown-linux-gnueabihf
-$(package)_sha256_hash_armv7-unknown-linux-gnueabihf:=5dd8b36467e03ba47bfa7ea5d7578c66bccb648dd2129d7cec6fb3ff00f81ca3
+$(package)_targets += armv7-unknown-linux-musleabihf
+$(package)_target_arm-unknown-linux-gnueabihf:=armv7-unknown-linux-musleabihf
+$(package)_target_armv7-unknown-linux-gnueabihf:=armv7-unknown-linux-musleabihf
+$(package)_sha256_hash_armv7-unknown-linux-musleabihf:=fbdb48968dd7af3a862c29e4e3ff85bcf333d97d21d262f347106542cc08b96d
 
 # Linux (ARMv8)
-$(package)_targets += aarch64-unknown-linux-gnu
-$(package)_target_aarch64-unknown-linux-gnu:=aarch64-unknown-linux-gnu
-$(package)_sha256_hash_aarch64-unknown-linux-gnu:=82b2308ee531775bf4d1faa57bddfae85f363bec43ca36ba6db4ebad7c1450d4
+$(package)_targets += aarch64-unknown-linux-musl
+$(package)_target_aarch64-unknown-linux-gnu:=aarch64-unknown-linux-musl
+$(package)_sha256_hash_aarch64-unknown-linux-musl:=991cc2f78d3db8fa1131ee2bb5807497e93e1efb9f447e2a7def0c4032ba4c54
 
 # Linux (PowerPC 64-bit little-endian)
-$(package)_targets += powerpc64le-unknown-linux-gnu
-$(package)_target_powerpc64le-unknown-linux-gnu:=powerpc64le-unknown-linux-gnu
-$(package)_sha256_hash_powerpc64le-unknown-linux-gnu:=142c7c2896fa4596b5c4c35d9d5e4d80acd5a699e5fa0560d92a89eda035ece3
+$(package)_targets += powerpc64le-unknown-linux-musl
+$(package)_target_powerpc64le-unknown-linux-gnu:=powerpc64le-unknown-linux-musl
+$(package)_sha256_hash_powerpc64le-unknown-linux-musl:=f6fad3f1c69acdd832ea2f487863f6428ba6e77b16c18e8db7fcd91b88e9e254
 
 # Linux (RISCV64GC)
-$(package)_targets += riscv64gc-unknown-linux-gnu
-$(package)_target_riscv64-unknown-linux-gnu:=riscv64gc-unknown-linux-gnu
-$(package)_target_riscv64gc-unknown-linux-gnu:=riscv64gc-unknown-linux-gnu
-$(package)_sha256_hash_riscv64gc-unknown-linux-gnu:=7b35c8207c77e3fc2f7f7a26dea989cc2cdc13a955851ff74d4882f96f4e14dd
+$(package)_targets += riscv64gc-unknown-linux-musl
+$(package)_target_riscv64-unknown-linux-gnu:=riscv64gc-unknown-linux-musl
+$(package)_target_riscv64gc-unknown-linux-gnu:=riscv64gc-unknown-linux-musl
+$(package)_sha256_hash_riscv64gc-unknown-linux-musl:=4a85e0c909d6a3202919638c3b95a496acdfec7e1245be1c406b7c1d26c32fba
 
 # Linux (x86_64)
-$(package)_targets += x86_64-unknown-linux-gnu
-$(package)_target_x86_64-unknown-linux-gnu:=x86_64-unknown-linux-gnu
-$(package)_sha256_hash_x86_64-unknown-linux-gnu:=e7e808b8745298369fa3bbc3c0b7af9ca0fb995661bd684a7022d14bc9ae0057
+$(package)_targets += x86_64-unknown-linux-musl
+$(package)_target_x86_64-unknown-linux-gnu:=x86_64-unknown-linux-musl
+$(package)_sha256_hash_x86_64-unknown-linux-musl:=3035f0c3ea9ae10ba1c21871c7a53cdb54a398616febffd42825965627a77216
 
 # macOS (ARMv8)
 $(package)_targets += aarch64-apple-darwin
 $(package)_target_aarch64-apple-darwin:=aarch64-apple-darwin
 $(package)_target_arm64-apple-darwin:=aarch64-apple-darwin
-$(package)_sha256_hash_aarch64-apple-darwin:=5ec28e75ed8715efaa2490d76ae026a34b13df6899d98b14d0a6995556f4e6b4
+$(package)_sha256_hash_aarch64-apple-darwin:=5d2fd6b5c3c482750074b6ab04443b1ec41ca824fddc814aab6a1fbcf5cfb53a
 
 # macOS (x86_64)
 $(package)_targets += x86_64-apple-darwin
 $(package)_target_x86_64-apple-darwin:=x86_64-apple-darwin
-$(package)_sha256_hash_x86_64-apple-darwin:=52084c8cdb34ca139a00f9f03f1a582d96b677e9f223a8d1aa31ae575a06cc16
+$(package)_sha256_hash_x86_64-apple-darwin:=b5111b105cfeb2772d92ca54e6f1c01d11def9c675c633f7d1ebdd09b83b0139
 
 # Windows (x86_64)
 $(package)_targets += x86_64-pc-windows-gnu
 $(package)_target_x86_64-w64-mingw32:=x86_64-pc-windows-gnu
-$(package)_sha256_hash_x86_64-pc-windows-gnu:=32d42270b114c9341e5bc9b1d24f336024889ddd32a7d22e4700cc3c45fe9d3d
+$(package)_sha256_hash_x86_64-pc-windows-gnu:=ae5c8942b3ccab5841c9ea65d1ac839c62553a763512799eb4c89de2ffad3d3e
 
 $(package)_file_name=rust-std-$($(package)_version)-$($(package)_target).tar.gz
 $(package)_sha256_hash=$($(package)_sha256_hash_$($(package)_target))
