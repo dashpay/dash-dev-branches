84069aeb2a44536485c618a74a9beafe42c013d2
Kittywhiskers Van Gogh <63189531+kwvg@users.noreply.github.com>
2026-01-23 01:23:16 +0530

    build: update to macOS 15.2 SDK from Xcode 16.0

--- ci/test/00_setup_env_mac.sh
+++ ci/test/00_setup_env_mac.sh
@@ -8,9 +8,9 @@ export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_macos_cross
 export HOST=x86_64-apple-darwin
-export PACKAGES="clang cmake lld llvm  zip"
-export XCODE_VERSION=15.0
-export XCODE_BUILD_ID=15A240d
+export PACKAGES="clang cmake lld llvm zip"
+export XCODE_VERSION=16.0
+export XCODE_BUILD_ID=16A242d
 export RUN_UNIT_TESTS=false
 export RUN_FUNCTIONAL_TESTS=false
 export GOAL="all deploy"
